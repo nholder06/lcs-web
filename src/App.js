c9d509a0c3f22201d1ea09338ee665262c8c6a13
Nicole Holder <nholder06@gmail.com>
2022-02-05 20:07:52 -0600

    WIP button styles

--- src/App.js
+++ src/App.js
@@ -2,19 +2,38 @@ import './App.css';
 import logo from './assets/LCS-logo.jpg';
 
 function App() {
+  const onMenuClick = e => {
+    e.preventDefault();
+    let count = 0;
+    count++;
+    console.log('Clicked ' + count + ' times.');
+  };
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} alt="logo" />
         <h1>Left Coast Standard</h1>
-        <a
+        {/* <a
           className="App-link"
           href="https://docs.google.com/document/d/1nJpkJJVUK-3MIiwyIOMhvmsy6IZpAwPUrlo4H80gkT0/edit?usp=sharing"
           target="_blank"
           rel="noopener noreferrer"
         >
           View Menu
-        </a>
+        </a> */}
+        <div>
+          <button
+            className="Menu-button"
+            type="button"
+            onClick={
+              // e.preventDefault();
+              onMenuClick
+              // ("window.location.href = 'http://google.com';");
+            }
+          >
+            View Menu
+          </button>
+        </div>
       </header>
     </div>
   );
