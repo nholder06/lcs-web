53e231758f3c4f4bb224d6b2aa0cab38b438d504
Nicole Holder <nholder06@gmail.com>
2022-07-09 19:50:58 -0500

    WIP-update styles

--- src/App.js
+++ src/App.js
@@ -1,22 +1,48 @@
 import './App.css';
-import logo from './assets/LCS-logo.jpg';
+import LazyLoad from 'react-lazyload';
+import logo from './assets/lcs-logo2.jpg';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} alt="logo" />
-        <h1>Left Coast Standard</h1>
-        <a
-          className="App-link"
-          // href="https://docs.google.com/document/d/1nJpkJJVUK-3MIiwyIOMhvmsy6IZpAwPUrlo4H80gkT0/edit?usp=sharing"
-          href="https://drive.google.com/file/d/1nJpkJJVUK-3MIiwyIOMhvmsy6IZpAwPUrlo4H80gkT0/view"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          View Menu
-        </a>
-      </header>
+      <div>
+        <nav>
+          <ul className="nav-bar">
+            <li className="nav-item">
+              {/* <LazyLoad> */}
+              <img src={logo} alt="logo" className="logo-image" />
+              {/* </LazyLoad> */}
+            </li>
+            <li className="nav-item">About</li>
+            <li className="nav-item">Menu</li>
+            <li className="nav-item">Contact</li>
+          </ul>
+        </nav>
+      </div>
+      <header className="App-header"></header>
+      <body className="main-body">
+        <h1 className="title">Left Coast Standard</h1>
+        <h2 className="mission-statement">
+          Striving to produce clean products without the use of any chemicals
+        </h2>
+        <div className="menu-link">
+          <a
+            // href="https://docs.google.com/document/d/1nJpkJJVUK-3MIiwyIOMhvmsy6IZpAwPUrlo4H80gkT0/edit?usp=sharing"
+            href="https://drive.google.com/file/d/1nJpkJJVUK-3MIiwyIOMhvmsy6IZpAwPUrlo4H80gkT0/view"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            View Menu
+          </a>
+        </div>
+      </body>
+      <div>
+        <footer className="footer">
+          <p className="footer-content">
+            Copyright &copy; 2022 Left Coast Standard.
+          </p>
+        </footer>
+      </div>
     </div>
   );
 }
